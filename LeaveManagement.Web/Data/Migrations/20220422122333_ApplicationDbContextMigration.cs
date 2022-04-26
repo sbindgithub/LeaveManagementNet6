0044794d4c46b09683ee62439f5e98162244ae0b
S Behera <saradabro@gmail.com>
2022-04-26 18:56:27 +0530

    Modified the CRUD screens and added datatables, added swal. End of Leavetype managament module.

---
+++ LeaveManagement.Web/Data/Migrations/20220422122333_ApplicationDbContextMigration.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagement.Web.Data.Migrations
+{
+    public partial class ApplicationDbContextMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "LeaveTypes",
+                newName: "DateModified");
+
+            migrationBuilder.RenameColumn(
+                name: "DateJoined",
+                table: "LeaveTypes",
+                newName: "DateCreated");
+
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "LeaveAllocations",
+                newName: "DateModified");
+
+            migrationBuilder.RenameColumn(
+                name: "DateJoined",
+                table: "LeaveAllocations",
+                newName: "DateCreated");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateModified",
+                table: "LeaveTypes",
+                newName: "DateOfBirth");
+
+            migrationBuilder.RenameColumn(
+                name: "DateCreated",
+                table: "LeaveTypes",
+                newName: "DateJoined");
+
+            migrationBuilder.RenameColumn(
+                name: "DateModified",
+                table: "LeaveAllocations",
+                newName: "DateOfBirth");
+
+            migrationBuilder.RenameColumn(
+                name: "DateCreated",
+                table: "LeaveAllocations",
+                newName: "DateJoined");
+        }
+    }
+}
